2453e36248aaf4922f5e211d6e002644405e3c79
gnardaar <111654982+gnardaar@users.noreply.github.com>
2022-12-04 14:27:20 -0700

    Add files via upload
    initail commit

---
+++ src/api/server.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+
+let Token = 'b8ff7c7245ca806d097fe6b10f06ab364c685f7e644e4a94';
+
+export const server_calls = {
+  get:async () => {
+    // const response = await Data
+  }
+}
+
---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export * from './Home';
+export * from '../api';
+export * from './Navbar';
+export * from './Phonebook';
+export * from './DataTable';
+export * from './Contact';
+export * from './About';
+
+
+
+
+
+
